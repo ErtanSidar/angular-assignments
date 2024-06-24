078e8cf1545200341adc539a853c71f45b8151a7
Essoft3 <essoft3@gmail.com>
2024-06-24 16:27:49 +0300

    basic assignment 1

---
+++ basics-assignment-1-start/src/app/alerts/success-alert/success-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles : `
+   p {
+     padding: 20px;
+     color: white;
+     background-color: green;
+     border : 1px solid black
+   }`
+})
+export class SuccessAlertComponent {
+
+}
---
+++ basics-assignment-1-start/src/app/alerts/warning-alert/warningalert.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector : "app-warning-alert",
+  templateUrl : "./warningalert.component.html",
+  styles : `p {
+    padding: 10px;
+    color : white;
+    background-color: red;
+    border : 1px solid black;
+  }
+  `
+})
+export class WarningAlertComponent {
+
+}
